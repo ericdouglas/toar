c3ee95f4db994ab026e224e665eccd1acd3918d5
Eric Douglas <eric.douglas.mail@gmail.com>
2015-07-04 17:35:41 -0300

    More tests and change on way that parameters are passed

--- index.js
+++ index.js
@@ -2,24 +2,42 @@
 
 module.exports = toar;
 
-function toar( input, splitter ) {
-  var array = [];
-  var key;
-  var type = typeof input;
- 
-  // Verify if input is an array
-  if ( input.constructor === Array ) {
-    return input;
-  }
+/*
+ * Convert any type/input to an array
+ *
+ * @param {Mix}    - input: to be formated
+ * @param {String} - splitter: used only when an input is a String
+ * @return {Array}
+ *
+ */
 
+function toar() {
+  var array     = [];
+  var input     = arguments[ 0 ];
+  var type      = typeof arguments[ 0 ];
+  var splitter  = arguments[ 1 ];
+  var key;
+ 
   // Verify if input is an object but is falsy (null or undefined)
   if ( type === 'object' && !input ) {
     return [ input ];
   }
 
-  if ( typeof type === 'object' || typeof type === 'string' ) {
+  // Verify if input is an array
+  if ( input.constructor === Array ) {
+    return input;
+  }
+
+  // Verify if input is a string
+  if ( typeof type === 'string' ) {
+    return input.split( splitter ); 
+  }
+
+  if ( typeof type === 'object' ) {
     for ( key in input ) {
-      array.push( key );
+      if ( input.hasOwnProperty( key )) {
+        array.push( key );
+      }
     }
   } else {
     array.push( input );
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,8 +5,57 @@ var toar   = require( '../' );
 
 describe( '#toar', function() {
 
-  it( 'Should return an array', function() {
-    expect( toar( [] )).to.be.an( 'array' );
-  });
+  var tests = [
+    {
+      description : '',
+      argument    : [],
+      result      : []
+    },
+    {
+      description : ' with one element "null"',
+      argument    : null,
+      result      : [ null ]
+    },
+    {
+      description : ' with one element "undefined"',
+      argument    : undefined,
+      result      : [ undefined ]
+    },
+    {
+      description : ' with three elements from a string',
+      argument    : 'javascript nodejs <3',
+      splitter    : ' ',
+      result      : [ 'javascript', 'nodejs', '<3' ]
+    },
+    {
+      description : ' with 3 keys from an object',
+      argument    : { name : 'User', lastName : 'lastName', userId : 'user13' },
+      result      : [ 'name', 'lastName', 'userId' ]
+    },
+    {
+      description : ' with a number',
+      argument    : 13,
+      result      : [ 13 ]
+    },
+    {
+      description : ' with an Infinity',
+      argument    : Infinity,
+      result      : [ Infinity ]
+    },
+    {
+      description : ' with a NaN',
+      argument    : NaN,
+      result      : [ NaN ]
+    },
+  ];
+
+  tests
+    .forEach( function( test ) {
+    
+      it( 'Should return an array' + test.description, function() {
+        expect( toar( test.argument, test.splitter )).to.deep.equal( test.result );
+      });
+    
+    });
 
 });
